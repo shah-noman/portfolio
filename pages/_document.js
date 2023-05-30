b75b3aa837349ce3189b06242f55e057683d9b9c
shah-noman <shah.noman7109@gmail.com>
2023-05-30 14:07:29 +0500

    set home

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ export default function Document() {
         />
         <meta name="ibthemes" content="ATFN" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.PNG" />
       </Head>
       <body>
         <Main />
